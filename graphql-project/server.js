dbb8ac57a02cf563845e6c1b11fe63dc55fc279a
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2024-09-18 20:24:55 +0200

    schema

--- graphql-project/server.js
+++ graphql-project/server.js
@@ -2,15 +2,22 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 const typeDefs = `#graphql
+
+    schema {
+        query: Query
+    }
+
     type Query {
         greeting: String,
+        imageUrl: String,
     }
 `;
 
 const resolvers = {
     Query: {
         greeting: () => 'Some text',
-    }
+        imageUrl: () => 'Some image url',
+    },
 }
 
 const server = new ApolloServer({ typeDefs, resolvers });
