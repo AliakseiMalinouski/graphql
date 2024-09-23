9c95b81782daee516a84fe66227783ec51900046
AliakseiMalinouski <aleksymalinowski21@gmail.com>
2024-09-23 19:45:06 +0200

    lists and assertions

--- graphql-project/resolvers.js
+++ graphql-project/resolvers.js
@@ -1,11 +1,11 @@
 const resolvers = {
     Query: {
         id: () => 'some-id',
-        params: () => ({
+        params: () => ([{
             x: 0,
             y: 0,
             z: 0,
-        }),
+        }]),
     }
 }
 
